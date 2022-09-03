d57e2764d9b67e3aee02c29d4b104e74c9d3ffb3
Lucas <lucasmvl2003@gmail.com>
2022-09-03 00:28:15 -0300

    adding typescrip in more files

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,9 +3,15 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useEffect, useState } from "react";
 
+interface Repository {
+    name: string,
+    description: string,
+    html_url: string
+}
 
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
 
 
     useEffect(() => {
@@ -24,7 +30,7 @@ export function RepositoryList() {
             
             <ul>
                 {repositories.map(repository => {
-                return <RepositoryItem key={repository.id} repository={repository}/>
+                    return <RepositoryItem key={repository.name} repository={repository}/>
                 })}    
             </ul>
         
